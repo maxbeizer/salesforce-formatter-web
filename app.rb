774915140be4675f2bd4cab258e32bdaf2102ee4
Jon Mountjoy <tech@jonmountjoy.com>
2015-10-27 13:04:45 +0000

    extend to handle multiple ids

--- app.rb
+++ app.rb
@@ -6,6 +6,22 @@ if development?
 end
 require_relative 'lib/formatter'
 
+helpers do
+  # parse a text area.  We want an array of IDs coming out.
+  # Do the right thing for newline or comma separated values.
+  def parse(t)
+    t.split(/[,\n]/).map(&:strip).select{ |x| !x.empty? }
+  end
+
+  # format a list of tuples
+  def oformat(ids)
+    out = ""
+    ids.each{ |i| out += "#{i[0]} : #{i[1]}\n"}
+    return out
+  end
+
+end
+
 get '/style.css' do
   scss :style
 end
@@ -15,7 +31,11 @@ get '/' do
 end
 
 post '/convert' do
-  @formatted = Formatter.format_15 params[:sfdc_id]
+  puts "GOT #{params[:sfdc_id]}"
+  ids = parse(params[:sfdc_id])
+
+  ids.map!{ |id| [id, (Formatter.format_15 id)] }
+  @formatted = oformat(ids)
   erb :index
 end
 
